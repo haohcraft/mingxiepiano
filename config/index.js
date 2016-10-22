fa028760cab95593df4d686e2007290cf9219fc1
haohcraft <hao.huang@datadoghq.com>
2016-10-22 15:21:08 -0400

    header profile img

--- config/index.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-const ENV = process.env.NODE_ENV || 'development';
-const configFile = require(`./${ENV}/index`);
-const config = ENV !== 'development' ? _.mapValues(configFile, (key) => process.env[key]) : configFile;
-
-export default config;
